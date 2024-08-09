4c8a078b5c583a480ad4c58e08c5bfe7eb3d8443
Geon <geonulim@gmail.com>
2024-08-09 16:09:18 +0900

    fix: 레이아웃 최상단으로 이동

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
+import Layout from 'components/Layout';
 import ListPage from 'pages/ListPage';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<ListPage />} />
-      </Routes>
-    </BrowserRouter>
+    <Layout>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<ListPage />} />
+        </Routes>
+      </BrowserRouter>
+    </Layout>
   );
 };
 
