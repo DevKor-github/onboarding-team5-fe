8f025af74c6a3d101023733303e57ec4f9458aa7
Geon <geonulim@gmail.com>
2024-08-15 14:58:48 +0900

    design: 채팅 목록 헤더 디자인 적용

--- src/components/headers/ListHeader.tsx
+++ src/components/headers/ListHeader.tsx
@@ -1,16 +1,6 @@
-import DefaultProfileImage from 'assets/images/default-profile.jpg';
-import { Link } from 'react-router-dom';
-
 const ListHeader = () => {
   return (
-    <header className='flex h-56 items-center justify-between border-b border-gray-100 px-20'>
-      <Link to='/' className='text-20 font-600'>
-        MOVIE
-      </Link>
-      <button className='relative h-28 w-28 overflow-hidden rounded-full'>
-        <img src={DefaultProfileImage} className='h-full w-full object-cover' />
-      </button>
-    </header>
+    <div className='flex h-48 items-center px-24 text-18 font-600'>채팅</div>
   );
 };
 
