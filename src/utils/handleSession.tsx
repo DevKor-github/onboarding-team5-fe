f6ed8928f1141a7b77e8d16c09107b7a199627ef
Geon <geonulim@gmail.com>
2024-08-15 21:04:06 +0900

    feat: 세션 유틸 함수 구현

---
+++ src/utils/handleSession.tsx
@@ -0,0 +1,37 @@
+import { Cookies } from 'react-cookie';
+
+interface SessionType {
+  accessToken: string;
+  refreshToken: string;
+  id: number;
+}
+
+const SESSION_TIME = 3600 * 1000 * 3; // 3시간
+
+const cookies = new Cookies();
+
+export const setSession = ({ accessToken, refreshToken, id }: SessionType) => {
+  const expires = new Date(Date.now() + SESSION_TIME);
+  const option = {
+    path: '/',
+    expires,
+  };
+
+  cookies.set('access_token', accessToken, option);
+  cookies.set('refresh_token', refreshToken, option);
+  cookies.set('id', id, option);
+};
+
+export const getSession = () => {
+  const id = cookies.get('id');
+  if (!id) {
+    return null;
+  }
+  return Number(id);
+};
+
+export const removeSession = () => {
+  cookies.remove('access_token');
+  cookies.remove('refresh_token');
+  cookies.remove('id');
+};
